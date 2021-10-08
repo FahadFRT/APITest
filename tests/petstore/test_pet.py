452cefd9b19497607051caa60a2f08c310a9cc9d
FahadFRT <m.fahad@fiveriverstech.com>
2021-10-08 13:06:42 +0500

    commit 6th

--- tests/petstore/test_pet.py
+++ tests/petstore/test_pet.py
@@ -10,3 +10,19 @@
 # @pytest.mark.run(order=2)
 # def test_pet_get(test_conf_pet_get):
 #     assert test_conf_pet_get.json()['name'] == "pet001"
+
+
+# import beautifulsoup4
+# f = open('reports/testng-results.html')
+# soup = BeautifulSoup4.BeautifulSoup(f)
+# f.close()
+# g = open('testng-results.xml', 'w')
+# print(g, soup.prettify())
+# g.close()
+
+import lxml
+from lxml import html, etree
+
+doc = html.fromstring(open('/home/muhammad/PycharmProjects/APITesting/reports/testng-results.html').read())
+out = open('/home/muhammad/PycharmProjects/APITesting/reports/testng-results.xml', 'wb')
+out.write(etree.tostring(doc))
