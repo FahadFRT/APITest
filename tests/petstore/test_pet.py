20db2828b24d29485c44a1a107752e3a187a6368
FahadFRT <m.fahad@fiveriverstech.com>
2021-10-08 12:43:46 +0500

    commit 5th

--- tests/petstore/test_pet.py
+++ tests/petstore/test_pet.py
@@ -1,12 +1,12 @@
-import pytest
-
-
-@pytest.mark.run(order=1)
-def test_pet_post(test_conf_pet_post):
-    assert test_conf_pet_post.json()['message'] == "1"
-
-
-
-@pytest.mark.run(order=2)
-def test_pet_get(test_conf_pet_get):
-    assert test_conf_pet_get.json()['name'] == "pet001"
+# import pytest
+#
+#
+# @pytest.mark.run(order=1)
+# def test_pet_post(test_conf_pet_post):
+#     assert test_conf_pet_post.json()['message'] == "1"
+#
+#
+#
+# @pytest.mark.run(order=2)
+# def test_pet_get(test_conf_pet_get):
+#     assert test_conf_pet_get.json()['name'] == "pet001"
